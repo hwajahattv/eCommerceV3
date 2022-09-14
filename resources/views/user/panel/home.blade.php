f3997015f81b0faabe55543a27965078f29946fe
Wajahat <h.wajahat.tv@gmail.com>
2022-09-14 11:44:41 +0500

    lara-ecommerce-wajahat1 deployment

--- resources/views/user/panel/home.blade.php
+++ resources/views/user/panel/home.blade.php
@@ -92,7 +92,7 @@
                     <div class="size-202 m-lr-auto respon4" style="min-height: 100px;">
                         <!-- Block1 -->
                         <div class="block1 wrap-pic-w">
-                            <img src="{{ asset('img/uploads/category/' . $cat->image) }}" alt="IMG-BANNER">
+                            <img src="{{ img / uploads / category / $cat->image }}" alt="IMG-BANNER">
                             <a href="{{ url('productsListOfCategory') . '/' . $cat->id }}"
                                 class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                                 <div class="block1-txt-child1 flex-col-l">
